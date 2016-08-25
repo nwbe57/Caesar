41f954f7242a0ae2e2bbc19e15ba877c4e698b6b
nwbe57 <nwbe57@yahoo.com>
2016-08-25 16:00:03 -0500

    first commit caesar

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 import webapp2
 import cgi
+from caesar import encrypt
 
 # html boilerplate for the top of every page
 page_header = """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Caesar Cypher</title>
-    <style type="text/css">
-        .error {
-            color: red;
-        }
-    </style>
+    <title>Caesar</title>
 </head>
 <body>
-    <h1>
-        <a href="/">Caeser Cypher</a>
-    </h1>
 """
 
 # html boilerplate for the bottom of every page
@@ -25,42 +18,74 @@ page_footer = """
 </html>
 """
 
-alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
-    "n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
 class Index(webapp2.RequestHandler):
+    """ Handles requests coming in to '/' (the root of our site)
+        e.g. www.Caesar.com/
+    """
 
     def get(self):
 
-            Rotate_by_form = """
-            <form action="/Rotate_by" method="post">
-                <label>
-                    Rotate by:
-                    <input type="text" name="rotate-by"/>
-                </label>
-            </form>
-            """
+        input_form = """
+        <form action="/input" method="post" style="padding-left: 30%; font-size: 20px;">
+            <label>
+                Rotate by:
+                <input type="int" name="rotation" style="width: 30px; font-size: 20px;"/>
+            </label>
+            <br>
+            <textarea input type="text" name="text"
+                    style="height: 100px; width: 500px; height: 300px; font-size: 20px;">
+            </textarea>
+            <br>
+            <label>
+                <input type="submit" value="Submit" style="font-size: 20px;"/>
+            </label>
+        </form>
+        """
 
-            Input_form = """
-            <form action="/Input_form" method="post">
-                <label>
-                    <input type="text" name="input-text">
-                </label>
-            </form>
-            """
-
-            Submit_form = """
-            <form action="/Submit_form" method="post">
-                <label>
-                    <input type="submit" name="submit-button">
-                </label>
-            </form>
-            """
-
-        main_content = Rotate_by_form + Input_form + Submit_form
-        response = page_header + main_content + page_footer
+        response = page_header + input_form + page_footer
         self.response.write(response)
 
+class Output(webapp2.RequestHandler):
+    """ Handles requests coming in to '/input'
+        e.g. www.Caesar.com/input
+    """
+
+    # def get(self):
+    #
+    #     output_form = """
+    #     <form action="/output" method="post" style="padding-left: 30%; font-size: 20px;">
+    #         <label>
+    #             Rotate by:
+    #             <input type="int" name="rotation" style="width: 30px; font-size: 20px;"/>
+    #         </label>
+    #         <br>
+    #         <textarea input type="text" name="usertext"
+    #                 style="height: 100px; width: 500px; height: 300px; font-size: 20px;">
+    #         </textarea>
+    #         <br>
+    #         <label>
+    #             <input type="submit" value="Submit" style="font-size: 20px;"/>
+    #         </label>
+    #     </form>
+    #     """
+    #
+    #     response = page_header + output_form + page_footer
+    #     self.response.write(response)
+
+    def post(self):
+
+        usertext = str(self.request.get("text"))
+        rotby = int(self.request.get("rotation"))
+
+        answer = encrypt(usertext, rotby)
+
+        self.response.write(answer)
+        #self.redirect('/output')
+        #usertext.replace(usertext, answer)
+
+
 app = webapp2.WSGIApplication([
-    ('/', Index)
+    ('/', Index),
+    ('/input', Output),
+    ('/output', Output)
 ], debug=True)
